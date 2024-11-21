7c0efe92f16ab9fadae528f22d5b7bc26b725661
magus <gimenezmathias2021@gmail.com>
2024-11-21 17:35:51 -0300

    Commit de prueba. la carpeta venv fue necesaria para poder importar pygame. nose porque no me dejaba importar

---
+++ constantes.py
@@ -0,0 +1,17 @@
+
+# Dimensiones de ventana
+ANCHO_VENTANA = 800
+ALTO_VENTANA = 600
+DIMENSION_VENTANA = (ANCHO_VENTANA, ALTO_VENTANA)
+
+# Titulos de ventanas
+TITULO_JUEGO = "Sudoku Clásico"
+TITULO_PUNTAJE = "Puntajes"
+TITULO_MENU = "Menu de inicio"
+
+# Colores
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+AZUL = (0, 122, 255)
+GRIS = (200, 200, 200)
+
---
+++ main.py
@@ -0,0 +1,3 @@
+import pygame, constantes as const
+
+
