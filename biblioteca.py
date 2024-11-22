3b6b2e98332679d7ce4054b6217f2eaa4c168b37
magus <gimenezmathias2021@gmail.com>
2024-11-22 15:35:45 -0300

    Se termino las funciones para generar el tablero de sudoku

--- biblioteca.py
+++ biblioteca.py
@@ -1,79 +1,150 @@
 import random, os
 os.system("cls")
 
-def crear_tablero(cantidad_filas:int, cantidad_columnas:int, valor_inicial:any) -> list:
+def crear_tablero(cantidad_filas: int, cantidad_columnas: int, valor_inicial: any) -> list:
     '''
-    Funcion que crea un tablero (matriz) pasando por paramtro las dimensiones.
+    Función que crea un tablero (matriz) pasando por parámetro las dimensiones.
+
+    Args:
+        cantidad_filas (int): Cantidad de filas que va a tener el tablero sudoku (9x9 o 16x16)
+        cantidad_columnas (int): Cantidad de columnas que va a tener el tablero sudoku (9x9 o 16x16)
+        valor_inicial (any): Valores que van a tener los valores de cada posicion del tablero. 
+    
+    Returns:
+        list: Tablero creado con las dimensiones especificadas 
+
+    Example:
+        >>> crear_tablero(9, 9, 0)
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]]
     '''
+
     tablero = []
 
-    for _ in range(cantidad_filas):
-        fila = [valor_inicial] * cantidad_columnas
+    for _ in range(filas):
+        fila = [valor_inicial] * columnas
         tablero += [fila]
     return tablero
 
-def validar_numero_en_tablero(tablero:list, fila:int, columna:int, numero:int) -> bool:
+def validar_numero_en_tablero(tablero:list, fila:int, columna:int, numero:int, subcuadrado_largo:int) -> bool:
     '''
-    Verifica si el numero puesto es valido en la fila y columna del tablero
+    Valida que el numero que se este ingresando en el tablero sea correcto respetando las reglas del sudoku.
+
+    Args:
+        tablero (list): tablero(matriz) ya creada que se usara para validar.
+        fila (int): numero de fila en la que ingrese el numero a validar.
+        columna (int): numero de columna en la que ingrese el numero a validar.
+        numero (int): numero que se ingrese para validar.
+        subcuadrado_largo: largo del subcuadrado que se usara para validar que el numero no se encuentre ahi.
+    
+    Returns:
+        bool: Si el numero es correcto sera True y en caso contrario False.
     '''
-    # Verifica si el numero esta en la fila
+
+    # Verifica si el numero se encuentra en la misma fila
     if numero in tablero[fila]:
         return False
 
-    # Verifica si el numero esta en la columna
+    # Verifica si el numero se encuentra en la misma columna
     for i in range(len(tablero)):
         if tablero[i][columna] == numero:
             return False
 
-    # Verifica si el numero esta en el cuadrado 3x3
-    subcuadro_fila = (fila // 3) * 3
-    subcuadro_col = (columna // 3) * 3
+    # Verifica si el numero se encuentra en el mismo subcuadro
+    subcuadro_fila = (fila // subcuadrado_largo) * subcuadrado_largo
+    subcuadro_col = (columna // subcuadrado_largo) * subcuadrado_largo
 
-    for i in range(subcuadro_fila, subcuadro_fila + 3):
-
-        for j in range(subcuadro_col, subcuadro_col + 3):
-
-            # Asegurarse de no comparar con la propia celda si ya está colocada
+    for i in range(subcuadro_fila, subcuadro_fila + subcuadrado_largo):
+        for j in range(subcuadro_col, subcuadro_col + subcuadrado_largo):
             if tablero[i][j] == numero:
                 return False
 
-    # Si pasa todas las verificaciones el numero es valido.
+    # Si se valido todo correctamente se retorna verdadero
     return True
 
-def llenar_tablero(tablero_vacio:list, desde:int, hasta:int) -> None:
+def llenar_tablero(tablero_vacio: list, subcuadrado_largo: int) -> bool:
     '''
-    Llena el tablero de forma aleatoria respetando las reglas del sudoku
+    Genera tableros con numeros aleatorios respetando las reglas del Sudoku.
+
+    Args:
+        tablero_vacio (list): Tablero creado para insertarle los numeros validos. 
+        subcuadrado_largo (int): Largo que va a tener los subcuadrados del tablero.
+    
+    Returns:
+        list: Tablero creado con las dimensiones especificadas 
+
+    Example:
+        >>> llenar_tablero(tablero, 3)
     '''
     for i in range(len(tablero_vacio)):
         for j in range(len(tablero_vacio[i])):
-            while True:
-                numero = random.randint(desde, hasta)
-                if validar_numero_en_tablero(tablero_vacio, i, j, numero):
-                    tablero_vacio[i][j] = numero
-                    break
+            if tablero_vacio[i][j] == 0: 
+                numeros = list(range(1, len(tablero_vacio) + 1))  
+                random.shuffle(numeros) 
 
-def mostrar_tablero(tablero:list) -> None:
+                for numero in numeros: 
+                    if validar_numero_en_tablero(tablero_vacio, i, j, numero, subcuadrado_largo):                        
+                        tablero_vacio[i][j] = numero  
+
+                        # Recursividad
+                        if llenar_tablero(tablero_vacio, subcuadrado_largo): 
+                            return True
+                        
+                        tablero_vacio[i][j] = 0  
+                return False  
+    return True  
+
+def mostrar_tablero(tablero: list, subcuadrado_largo: int) -> None:
     '''
-    Muestra la simulacion de un tablero en consola
+    Muestra la simulacion de un tablero sudoku por consola.
+
+    Args:
+        tablero (list): tablero(matriz) ya generada para mostrar por consola de forma de tablero sudoku.
+        subcuadrado_largo (int): Largo de los subcuadrados para mostrar dinamicamente por consola respetando las dimensiones del tablero.
+    
+    Returns:
+        None: No retorna nada.
+
+    Example:
+        >>> mostrar_tablero(tablero, 3)
     '''
     for i in range(len(tablero)):
-
-        # separadores de filas
-        if i % 3 == 0 and i != 0:
-            print("-" * 21)
+        # Separadores de filas cada subcuadrado
+        if i % subcuadrado_largo == 0 and i != 0:
+            print("---" * (len(tablero) + subcuadrado_largo - 2))
 
         for j in range(len(tablero[i])):
-
-            # separadores de columnas
-            if j % 3 == 0 and j != 0:
+            # Separadores de columnas cada subcuadrado
+            if j % subcuadrado_largo == 0 and j != 0:
                 print("|", end=" ")
 
-            # Muestra el numero
-            print(f"{tablero[i][j]} ", end="")
+            print(f"{tablero[i][j]:2} ", end="")
 
-        # salto en linea por cada fila
         print()
 
-tablero = crear_tablero(9, 9, 0)
-llenar_tablero(tablero, 1, 9)
-mostrar_tablero(tablero)
+
+# Sudoku 9x9
+filas = 9
+columnas = 9
+valor_inicial = 0
+subcuadrado_largo = 3  
+
+
+tablero = crear_tablero(filas, columnas, valor_inicial)
+
+print("Tablero vacio:\n")
+mostrar_tablero(tablero, subcuadrado_largo)
+
+print("\nGenerando Sudoku dinamico...\n")
+llenar_tablero(tablero, subcuadrado_largo)
+
+print("Sudoku completado:\n")
+mostrar_tablero(tablero, subcuadrado_largo)
+print()
