9b6c9a7518c1c769531ff95e529ff3caa51d3b02
magus <gimenezmathias2021@gmail.com>
2024-11-24 23:56:55 -0300

    Se implemento la ventana sudoku (en otro archivo. despues se pasa a main) y hice la funcion de dibujar el tablero responsive

--- constantes.py
+++ constantes.py
@@ -1,21 +1,21 @@
-
-# Dimensiones de ventana
+### DIMENSIONES DE LAS VENTANAS ###
 ANCHO_VENTANA = 800
 ALTO_VENTANA = 600
 DIMENSION_VENTANA = (ANCHO_VENTANA, ALTO_VENTANA)
 
-# Titulos de ventanas
+### TITULOS DE LAS VENTANAS ###
 TITULO_JUEGO = "Sudoku Clásico"
 TITULO_PUNTAJE = "Puntajes"
 TITULO_MENU = "Sudoku Clásico - Menú"
 
-# Colores
+### COLORES ###
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 AZUL = (0, 122, 255)
-GRIS = (200, 200, 200)
+GRIS_CLARO = (200, 200, 200)
+AZUL_CLARO = (173, 216, 230)
 
-#Imagenes
+### IMAGENES ###
 FONDO_MENU_PRINCIPAL = "imagenes/menu_principal.png"
 LETRA_MENU_PRINCIPAL = "imagenes/fuente.ttf"
 
